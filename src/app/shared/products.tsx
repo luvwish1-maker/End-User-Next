6cf3204575fedfe2c093f7769b585cc1c5dce623
kuriakose <kuriakose00@gmail.com>
2025-10-20 15:14:04 +0530

    style fixes

--- src/app/shared/products.tsx
+++ src/app/shared/products.tsx
@@ -37,9 +37,13 @@ export default function Products() {
 
     const settings = {
         dots: false,
-        infinite: false,
+        infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 1,
+        slidesToScroll: 2,
+        swipeToSlide: true,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 4000,
         responsive: [
             { breakpoint: 1024, settings: { slidesToShow: 2 } },
             { breakpoint: 700, settings: { slidesToShow: 1 } },
